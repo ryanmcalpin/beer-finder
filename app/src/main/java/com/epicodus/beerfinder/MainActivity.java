89fdc2aa5133892cd52165aef93627c7facba4fd
Guest User <Guest@Epicodus-5D.local>
2017-05-26 10:00:26 -0700

    add basic layout for MainActivity

--- app/src/main/java/com/epicodus/beerfinder/MainActivity.java
+++ app/src/main/java/com/epicodus/beerfinder/MainActivity.java
@@ -1,13 +1,32 @@
 package com.epicodus.beerfinder;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.searchButton) Button searchButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        searchButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == searchButton) {
+//            Intent intent = new Intent(MainActivity.this, )
+            Toast.makeText(MainActivity.this, "test", Toast.LENGTH_SHORT).show();
+        }
     }
 }
