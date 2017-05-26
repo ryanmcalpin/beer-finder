735fe09bf0b7799ed23a3feaf7acc0b91d63e0ce
Ryan McAlpin <rhmcalpin@gmail.com>
2017-05-26 11:26:12 -0700

    add title font

--- app/src/main/java/com/epicodus/beerfinder/MainActivity.java
+++ app/src/main/java/com/epicodus/beerfinder/MainActivity.java
@@ -1,10 +1,12 @@
 package com.epicodus.beerfinder;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import butterknife.Bind;
@@ -13,12 +15,16 @@ import butterknife.ButterKnife;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     @Bind(R.id.searchBeerButton) Button beerButton;
     @Bind(R.id.searchStyleButton) Button styleButton;
+    @Bind(R.id.titleView) TextView titleView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        Typeface titleFont = Typeface.createFromAsset(getAssets(), "fonts/Pacifico.ttf");
+        titleView.setTypeface(titleFont);
 
         beerButton.setOnClickListener(this);
         styleButton.setOnClickListener(this);
